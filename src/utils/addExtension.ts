c8085691ac66ad61859205d68868411a99c2a59e
laowan <11513313+laowans@user.noreply.gitee.com>
2023-08-14 03:08:45 +0800

    优化代码逻辑，添加注释

--- src/utils/addExtension.ts
+++ src/utils/addExtension.ts
@@ -8,29 +8,22 @@ export function addExtension(importPath: string, ext: string[]) {
 	const exists = fs.existsSync(importPath);
 	const isDir = exists ? fs.statSync(importPath).isDirectory() : false;
 	const result = {
-		change: false,
-		newImportPath: importPath,
+		change: false, // 路径是否改变
+		newImportPath: importPath, // 新的导入路径
 	};
 
-	if (!(exists && !isDir)) {
-		if (exists && isDir) {
-			for (let i = 0; i < ext.length; i++) {
-				const extPath = path.join(importPath, 'index.' + ext[i]);
+	// 排除导入路径存在且不为文件夹的情况
+	if (!exists || isDir) {
+		for (let i = 0; i < ext.length; i++) {
+			// 默认加上 `.${ext}`
+			let extPath: string = importPath + '.' + ext[i];
+			// 路径存在且为文件夹，则组合路径 `index.${ext}`
+			if (exists && isDir) extPath = path.join(importPath, 'index.' + ext[i]);
 
-				if (fs.existsSync(extPath)) {
-					result.change = true;
-					result.newImportPath = extPath;
-					break;
-				}
-			}
-		} else {
-			for (let i = 0; i < ext.length; i++) {
-				const extPath = importPath + '.' + ext[i];
-
-				if (fs.existsSync(extPath)) {
-					result.change = true;
-					result.newImportPath = extPath;
-				}
+			if (fs.existsSync(extPath)) {
+				result.change = true;
+				result.newImportPath = extPath;
+				break;
 			}
 		}
 	}
